c2c4d54d86c932551a0e49a5065cd0eaf11c9fc4
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-04 20:29:52 +0530

    first commit

---
+++ src/test/java/com/ec7205/event_hub/auth_service_api/AuthServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec7205.event_hub.auth_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
