3640ed01c57bcdfc3f64ccfa77d6d56065f86df5
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-16 16:37:13 +0530

    refactor: Remove EmailService interface as part of service consolidation

--- src/main/java/com/ec7205/event_hub/auth_service_api/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.ec7205.event_hub.auth_service_api.service;
-
-import java.io.IOException;
-
-public interface EmailService {
-    boolean sendUserSignupVerificationCode(String toEmail, String subject, String otp,String firstName) throws IOException;
-    public boolean sendHostPassword(String toEmail, String subject, String password, String firstName) throws IOException;
-}
