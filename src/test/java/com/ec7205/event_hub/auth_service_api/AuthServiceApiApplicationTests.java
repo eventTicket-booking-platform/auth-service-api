e06355eac82a5e092cd062db9d89cb26ea3bd95a
AmiruHoradagoda <amirumithsara1234@gmail.com>
2026-04-27 22:39:55 +0530

    feat: Add H2 database dependency and test configuration properties for integration testing

--- src/test/java/com/ec7205/event_hub/auth_service_api/AuthServiceApiApplicationTests.java
+++ src/test/java/com/ec7205/event_hub/auth_service_api/AuthServiceApiApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ec7205.event_hub.auth_service_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.ec7205.event_hub.auth_service_api.service.SystemUserService;
+import com.ec7205.event_hub.auth_service_api.utils.PasswordGenerator;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApiApplicationTests {
+
+	@Autowired
+	private AuthServiceApiApplication application;
+
+	@MockBean
+	private SystemUserService systemUserService;
+
+	@MockBean
+	private PasswordGenerator passwordGenerator;
 
 	@Test
 	void contextLoads() {
+		// Verifies the application context starts with test-safe infrastructure settings.
 	}
 
 }
